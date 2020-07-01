edf7bc7a3463da8b47a7e0ffb339f385d6e7df01
amaansingh2006 <64639335+amaansingh2006@users.noreply.github.com>
2020-07-01 19:31:45 +0530

    Add files via upload

---
+++ sketch.js
@@ -0,0 +1,23 @@
+var canvas, backgroundImage;
+
+var gameState = 0;
+var playerCount;
+var allPlayers;
+var distance = 0;
+var database;
+
+var form, player, game;
+
+var cars, car1, car2, car3, car4;
+var track, player1, player2, player3, player4, ground;
+
+function preload(){
+}
+
+function setup(){
+  canvas = createCanvas(displayWidth - 20, displayHeight-30);
+}
+
+
+function draw(){
+}
